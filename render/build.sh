c348fffc87f7d5d30fee738c0385dedb6141398d
vp20007 <91637197+vp20007@users.noreply.github.com>
2023-10-26 18:35:05 -0600

    build

--- render/build.sh
+++ render/build.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 set -o errexit
 
 pip install -r requirements.txt 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py tailwind build
